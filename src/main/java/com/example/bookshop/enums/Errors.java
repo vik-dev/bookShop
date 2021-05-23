7143b8d820210538a785d3bc50b6f55cb7c099e9
Viktor <kvitek2020@gmail.com>
2021-05-24 00:06:59 +0400

    errors list

--- src/main/java/com/example/bookshop/enums/Errors.java
+++ src/main/java/com/example/bookshop/enums/Errors.java
@@ -1,20 +1,25 @@
 package com.example.bookshop.enums;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
 public enum Errors {
-    BOOK_NOT_FOUND(1),
-    UNEXPECTED_EXCEPTION(2),
-    BOOK_ALREADY_EXISTS(3),
-    NOT_ENOUGH_MONEY(4),
-    USER_ALREADY_EXISTS(5),
-    USER_NOT_FOUND(6);
+    BOOK_NOT_FOUND(1, "Книга не найдена"),
+    UNEXPECTED_EXCEPTION(2, "Неизвестная ошибка"),
+    BOOK_ALREADY_EXISTS(3, "Книга уже существует"),
+    NOT_ENOUGH_MONEY(4, "Не достаточно денег"),
+    USER_ALREADY_EXISTS(5, "Пользователь уже существует"),
+    USER_NOT_FOUND(6, "Пользователь не найден");
 
-    Errors(int code) {
+    Errors(int code, String description) {
         this.code = code;
+        this.description = description;
     }
 
     private final int code;
+
+    @Getter
+    private final String description;
 
     @JsonValue
     public int getCode() {
