cab81da90e5288df5b170304e393efc6a335040d
Viktor <kvitek2020@gmail.com>
2021-05-24 02:36:29 +0400

    Cors off

--- src/main/java/com/example/bookshop/configuration/WebConfig.java
+++ src/main/java/com/example/bookshop/configuration/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
             }
         };
     }
